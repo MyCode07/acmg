d55c30e68e4ec732ae9bd647abb8ffcef31d0d0b
MyCode07 <erem19992015@mail.ru>
2026-02-22 22:00:19 +0300

    first log

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+import "./parts/popup.js";
+import "./parts/menu.js";
+import "./parts/tabs.js";
+import "./parts/sliders.js";
+import "./parts/select.js";
+import "./parts/filter.js";
+
+import { maskInputs } from "./static/inputmask.js";
+import { stickyHeader } from "./parts/header.js";
+import { Fancybox } from "@fancyapps/ui";
+import { replaceDomElements } from "./static/replace.js";
+
+replaceDomElements()
+maskInputs('+7 (999) 999-99-99', '._mask-phone')
+stickyHeader()
+Fancybox.bind("[data-fancybox]", {
+});
+
+document.addEventListener('click', function (e) {
+    let targetEl = e.target;
+    if (targetEl.classList.contains('pages-close')) {
+        document.querySelector('.pages').classList.toggle('_hide');
+    }
+})
